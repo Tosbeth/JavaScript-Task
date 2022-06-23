5b4da79dfce4c6eb8f14021824c1234f8ca3169e
Tosbeth <105288291+Tosbeth@users.noreply.github.com>
2022-06-23 17:30:28 +0100

    Files added via upload
    A JavaScript code that prints out my name, height and country.

---
+++ script.js
@@ -0,0 +1,7 @@
+let myName ='Name: Oluwatomisin Elizabeth Oyediran'; 
+ let myHeight ='Height: 157cm'; 
+ let myCountry ='Country: Nigeria'; 
+  
+ alert(myName); 
+ alert(myHeight); 
+ alert(myCountry);
