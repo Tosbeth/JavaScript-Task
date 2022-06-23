1b994b4d2d75b277a31469406bdc0831c4b671a5
Tosbeth <105288291+Tosbeth@users.noreply.github.com>
2022-06-23 21:05:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let myName ='Name: Oluwatomisin Elizabeth Oyediran'; 
- let myHeight ='Height: 160
-cm'; 
+ let myHeight ='Height: 160cm'; 
  let myCountry ='Country: Nigeria'; 
   
  alert(myName); 
